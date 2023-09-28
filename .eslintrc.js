75eeb68e7ab0ee856626dfdb2853e6af08f1b2c1
Kiki-Q <dingqilove@iCloud.com>
2023-09-29 01:53:40 +0800

    feat: add eslint包

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // 自定义测试：0是忽略，1是警告，2是报错
+    // quotes: 2,
+    // semi: 1,
+    // 'no-console': 1,
+    // 'space-before-function-paren': 0,
+  },
+};
