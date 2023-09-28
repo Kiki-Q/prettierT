1aa2dfe2d6a8492a529b08e3d73fe7f6a48b176c
Kiki-Q <dingqilove@iCloud.com>
2023-09-29 03:57:55 +0800

    feat: add pre-commit

--- test.js
+++ test.js
@@ -1,15 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import moduleName from "module"
-// eslint-disable-next-line no-undef
-console.log(b)
-function name(params) {}
-const a = "123"
-const b = "456"
+console.log(b);
+function name() {}
+// const a = '123';
+const b = '456';
 
-const c = { o: 1 }
-export default name
-
-// feat: husky + fix missing lint + prettier + esint
-
-// "lint": "eslint --fix --ext .js,.vue",
-// "format": "prettier --write \"./**/*.{html,vue,ts,js,json,md,less,sass,scss,sty}\""
+// const c = { o: 1 };
+export default name;
