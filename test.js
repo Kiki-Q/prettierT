27db18626fe7e2df6b6cf74b7e88872a6c717a58
Kiki-Q <dingqilove@iCloud.com>
2023-09-29 00:08:34 +0800

    feat: all

---
+++ test.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import moduleName from "module"
+// eslint-disable-next-line no-undef
+console.log(b)
+function name(params) {}
+const a = "123"
+const b = "456"
+
+const c = { o: 1 }
+export default name
+
+// feat: husky + fix missing lint + prettier + esint
+
+// "lint": "eslint --fix --ext .js,.vue",
+// "format": "prettier --write \"./**/*.{html,vue,ts,js,json,md,less,sass,scss,sty}\""
